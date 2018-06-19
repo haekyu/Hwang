b4f648f610ee11705d1a3c599022080bcd4f0dbe
haekyu <hkpark627@gmail.com>
2018-06-19 21:37:59 +0900

    0619

---
+++ 0619/0619.py
@@ -0,0 +1,61 @@
+lst = [0, 1, 2, 3, 1, 2, 3, 4]
+
+for e in lst:
+    if e > 2:
+        continue
+    print(e)
+    
+# # default dictionary
+# from collections import defaultdict
+
+# d = defaultdict(lambda x: ord(x) - ord('a'))
+# d = defaultdict(lambda: 0)
+
+# # f(x) = 
+
+
+# # for i in lst:
+# #     if i not in d:
+# #         d[i] = 0
+# # # d = {'a': 0, 'b': 0, 'c': 0}
+
+# # for i in lst:
+# #     d[i] += 1
+
+
+# name = ['철수', '영희', '영수']
+# age = [1, 2, 3]
+# addr = ['a', 'b', 'c']
+
+
+# chul_idx = name.index('철수')
+# chul_name, chul_age, chul_addr = name[chul_idx], age[chul_idx], addr[chul_idx]
+
+
+
+# for total in zip(name, age, addr):
+#     print(total)
+#     n = total[0]
+#     a = total[1]
+#     aa = total[2]
+#     print(n, a, aa)
+
+# # break
+
+# for 
+
+
+
+
+# # lst1 = [1, 2, 3, 4]
+# # lst2 = [32, 3, 2, 1]
+# # lst3 = [2, 3, 2, 0]
+
+# # # for i in lst:
+# # #     print(i)
+
+# # for th, i in enumerate(lst):
+# #     print(th, i)
+# #     if i is None:
+# #         break
+
