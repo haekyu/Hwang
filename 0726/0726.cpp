ae1e879b876d0fae51c00ccbaa858b7f77a8270b
haekyu <hkpark627@gmail.com>
2018-07-26 14:41:23 +0900

    0726

--- 0726/0726.cpp
+++ 0726/0726.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main(){
-	cout << "char: " << sizeof(char) << " bytes\n";
-	return 0;
+    cout << "char: " << sizeof(char) << " bytes\n";
+    cout << 5 / 3 << "\n";
+    string myString = "abcd";
+    cout << myString << "\n";
+
+    string s = "ABC";
+    char c = 'd';
+    string long_string = s + c;
+    cout << long_string << "\n";
+    cout << long_string.size() << "\n";
+    return 0;
 }
